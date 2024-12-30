c450562f7545022eb4c9f849a55bddc5fec07167
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2024-12-30 09:45:24 +0700

    Tambah Dependency Injection untuk MahasiswaRepository

---
+++ app/src/main/java/com/example/project26des/dependenciesinjection/MahasiswaContainer.kt
@@ -0,0 +1,31 @@
+package com.example.project26des.dependenciesinjection
+
+import com.example.project26des.repository.MahasiswaRepository
+import com.example.project26des.repository.NetworkMahasiswaRepository
+import com.example.project26des.service.MahasiswaService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainer {
+    val kontakRepository: MahasiswaRepository
+}
+
+class MahasiswaContainer : AppContainer {
+
+    private val baseUrl = "http://10.0.2.2:8000/umyTI/" // localhost diganti ip kalau run di hp
+    private val json = Json { ignoreUnknownKeys = true }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl)
+        .build()
+
+    private val mahasiswaService: MahasiswaService by lazy {
+        retrofit.create(MahasiswaService::class.java)
+    }
+
+    override val kontakRepository: MahasiswaRepository by lazy {
+        NetworkMahasiswaRepository(mahasiswaService)
+    }
+}
